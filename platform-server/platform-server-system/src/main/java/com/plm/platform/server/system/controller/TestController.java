97507be7115668c24c30a3383f79771989c11737
chenwenhua666 <764471698@qq.com>
2020-09-18 17:14:58 +0800

    resource_ids

--- platform-server/platform-server-system/src/main/java/com/plm/platform/server/system/controller/TestController.java
+++ platform-server/platform-server-system/src/main/java/com/plm/platform/server/system/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.plm.platform.server.system.controller;
 
+import com.plm.platform.common.core.entity.PlatformResponse;
 import com.plm.platform.common.core.entity.system.TradeLog;
 import com.plm.platform.server.system.service.ITradeLogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,11 @@ public class TestController {
     @PostMapping("package/send")
     public void packageSend(@RequestBody TradeLog tradeLog) {
         this.tradeLogService.packageAndSend(tradeLog);
+    }
+
+    @GetMapping("scope/test")
+    @PreAuthorize("#oauth2.hasScope('write')")
+    public PlatformResponse testScope() {
+        return new PlatformResponse().message("当前client包含write scope");
     }
 }
